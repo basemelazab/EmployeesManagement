25b0b006015b2a5ebd9073a9773344ecf1fa8598
basemelazab <basem.elazab2014@gmail.com>
2023-02-25 02:47:13 +0200

    Add project files.

---
+++ EmployeeManagement/Helpers/CryptoService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace EmployeeManagement.Helpers
+{
+    public class CryptoService
+    {
+        private const int saltSize = 32;
+        public static byte[] GenerateSalt()
+        {
+            using(var rng = new RNGCryptoServiceProvider())
+            {
+                var RandomNumber=new byte[saltSize];
+                rng.GetBytes(RandomNumber);
+                return RandomNumber;
+            }
+        }
+        public static byte[] ComputeHMAC256(byte[]data, byte[] salt)
+        {
+            using(var hmac = new HMACSHA256(salt))
+            {
+                return hmac.ComputeHash(data);
+            }
+        }
+    }
+}
---
+++ EmployeeManagement/ViewModel/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace EmployeeManagement.ViewModel
+{
+    public class UserViewModel
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "User Name Is Required")]
+        public string UserName { get; set; }
+        [Display(Name = "User Email")]
+        [Required(ErrorMessage = "User Email Is Required")]
+        public string UserEmail { get; set; }
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name Is Required")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name Is Required")]
+        public string LastName { get; set; }
+        [Display(Name = "User Password")]
+        [Required(ErrorMessage = "User Password Is Required")]
+        public string UserPassword { get; set; }
+        [Display(Name = "Comfirm Password")]
+        [Required(ErrorMessage = "UComfirm Password Is Required")]
+        public string ComfirmPassword { get; set; }
+
+    }
+}
